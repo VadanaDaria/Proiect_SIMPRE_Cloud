0d5a35c6728e6c64b5cfeebcefdd2d3156f43709
VadanaDaria <vadanaiulia20@stud.ase.ro>
2024-05-08 17:59:32 +0300

    Finished

--- components/RecordForm.jsx
+++ components/RecordForm.jsx
@@ -53,32 +53,32 @@ const RecordForm = (props) => {
         </div>
         <div>
           <label
-            htmlFor="Place(Country)"
+            htmlFor="place"
             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
-            Place(Country)
+            Place
           </label>
           <textarea
-            id="Place(Country)"
+            id="place"
             rows="4"
             value={data.place}
-            onChange={(e) => handleChange("Place(Country)", e.target.value)}
+            onChange={(e) => handleChange("place", e.target.value)}
             className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="In what country is it?"
           ></textarea>
         </div>
         <div>
           <label
-            htmlFor="Visited)"
+            htmlFor="visited"
             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
             Visited
           </label>
           <textarea
-            id="Visited"
+            id="visited"
             rows="4"
             value={data.visited}
-            onChange={(e) => handleChange("Visited", e.target.value)}
+            onChange={(e) => handleChange("visited", e.target.value)}
             className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Did you visited this place?"
           ></textarea>
@@ -89,7 +89,7 @@ const RecordForm = (props) => {
           <button
             type="button"
             onClick={handleCancel}
-            className="button" class="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+            className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
           >
             Cancel
           </button>
